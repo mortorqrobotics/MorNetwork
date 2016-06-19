85f2fb593bdd2ecd9467325050fd00b5fd8db940
afuous <benheller42@gmail.com>
2016-06-19 09:23:32 -0700

    Switch to team _id instead of team id in schemas

--- models/Subdivision.js
+++ models/Subdivision.js
@@ -1,24 +1,29 @@
-var mongoose = require('mongoose');
-Schema = mongoose.Schema;
+"use strict";
 
-var subdivisionSchema = new Schema({
-	name:        { type: String, required: true },
-	type:        { type: String, required: true },
-	team:        { type: String, required: true },
-	created_at:  Date,
-	updated_at:  Date,
-});
+module.exports = function(mongoose) {
 
-subdivisionSchema.pre('save', function(next){
-	now = new Date();
-	this.updated_at = now;
-	if ( !this.created_at ) {
-		this.created_at = now;
-	}
-	next();
-});
+	let Schema = mongoose.Schema;
+	let ObjectId = Schema.Types.ObjectId;
 
-module.exports = function(db) {
-	var Subdivision = db.model('Subdivision', subdivisionSchema);
+	let subdivisionSchema = new Schema({
+		name:        { type: String, required: true },
+		type:        { type: String, required: true },
+		team:        { type: ObjectId, ref: "Team", required: true },
+		created_at:  Date,
+		updated_at:  Date,
+	});
+
+	subdivisionSchema.pre("save", function(next){
+		let now = new Date();
+		this.updated_at = now;
+		if ( !this.created_at ) {
+			this.created_at = now;
+		}
+		next();
+	});
+
+	let Subdivision = mongoose.model("Subdivision", subdivisionSchema);
+
 	return Subdivision;
+
 };
