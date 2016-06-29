96b600103d0e9ace65cec9fb54e33667fc2a2880
E-A-A <ev34an@gmail.com>
2016-06-29 04:54:48 +0000

    Add update normalGroups middleware

--- src/models/NormalGroup.js
+++ src/models/NormalGroup.js
@@ -56,11 +56,44 @@ module.exports = function() {
     normalGroupSchema.pre("save", Promise.coroutine(function*(next) {
         let group = this;
 
-        if (normalGroup.isModified("users")) {
+        if (group.isModified("users")) {
             group.updateMembers();
         }
 
         next();
+    }));
+    normalGroupSchema.path("groups").set(function(){ // TODO: cause trigger on push from server
+        let group = this;
+        group.oldGroups = group.groups;
+    });
+    
+    normalGroupSchema.pre("save", Promise.coroutine(function*(next){
+        let group = this;
+        
+        if(!group.isModified("groups")){
+            return next();
+        }
+        for(let i = 0; i < group.groups.length; i++){
+
+               if(group.oldGroups.indexOf(group.groups[i]) === -1){
+                   let groups = yield Group.find({_id: group.groups[i]});
+                   for(let j of groups){
+                   j.dependentGroups.push(group.groups[i]);
+                   j.save();
+                   }
+               }
+        }
+          for(let i = 0; i < group.oldGroups.length; i++){   
+               
+               if(group.groups.indexOf(group.oldGroups[i]) === -1){
+                    let groups = yield Group.find({_id: group.oldGroups[i]});
+                    for(let iteratedGroup of groups){
+                    iteratedGroup.dependentGroups.splice(group.oldGroups.indexOf(group.oldGroups[i]),1);
+                    iteratedGroup.save();
+                    }
+            }
+        }
+      next();
     }));
 
     let NormalGroup = Group.discriminator("NormalGroup", normalGroupSchema);
