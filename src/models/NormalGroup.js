7a5974e90f0e2acc69e948a7c3e70a6fd5964e47
AriellaS <ariella.simoni@gmail.com>
2016-06-29 16:55:20 -0700

    Fix updating dependent groups

--- src/models/NormalGroup.js
+++ src/models/NormalGroup.js
@@ -62,42 +62,47 @@ module.exports = function() {
 
         next();
     }));
-    normalGroupSchema.path("groups").set(function(){ // TODO: cause trigger on push from server
+
+    normalGroupSchema.path("groups").set(function() { // TODO: cause trigger on push from server
         let group = this;
         group.oldGroups = group.groups;
     });
-    
-    normalGroupSchema.pre("save", Promise.coroutine(function*(next){
+
+    normalGroupSchema.pre("save", Promise.coroutine(function*(next) {
         let group = this;
-        
-        if(!group.isModified("groups")){
+
+        if (!group.isModified("groups")) {
             return next();
         }
-        for(let i = 0; i < group.groups.length; i++){
 
-               if(group.oldGroups.indexOf(group.groups[i]) === -1){
-                   let groups = yield Group.find({_id: group.groups[i]});
-                   for(let j of groups){
-                   j.dependentGroups.push(group.groups[i]);
-                   j.save();
-                   }
-               }
-        }
-          for(let i = 0; i < group.oldGroups.length; i++){   
-               
-               if(group.groups.indexOf(group.oldGroups[i]) === -1){
-                    let groups = yield Group.find({_id: group.oldGroups[i]});
-                    for(let iteratedGroup of groups){
-                    iteratedGroup.dependentGroups.splice(group.oldGroups.indexOf(group.oldGroups[i]),1);
+        for (let i = 0; i < group.groups.length; i++) {
+            if (group.oldGroups.indexOf(group.groups[i]) === -1) {
+                let groups = yield Group.find({
+                    _id: group.groups[i]
+                });
+                for (let iteratedGroup of groups) {
+                    iteratedGroup.dependentGroups.push(group);
                     iteratedGroup.save();
-                    }
+                }
             }
         }
-      next();
+        for (let i = 0; i < group.oldGroups.length; i++) {
+            if (group.groups.indexOf(group.oldGroups[i]) === -1) {
+                let groups = yield Group.find({
+                    _id: group.oldGroups[i]
+                });
+                for (let iteratedGroup of groups) {
+                    let dependents = iteratedGroup.dependentGroups;
+                    dependents.splice(dependents.indexOf(group), 1);
+                    iteratedGroup.save();
+                }
+            }
+        }
+        next();
     }));
 
     let NormalGroup = Group.discriminator("NormalGroup", normalGroupSchema);
 
-    return NormalGroup; 
+    return NormalGroup;
 
 };
